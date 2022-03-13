0d18d789d67df32cdce358245bc1103c22664a2f
DapinvaX <dapinva95@gmail.com>
2022-03-13 17:34:50 +0100

    Actualización de modificar

--- modificarDatoRegistroPDO.php
+++ modificarDatoRegistroPDO.php
@@ -1,63 +1,68 @@
 <?php
 
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+
+echo 'Editar Registro';
+
+
+
 require ('./DAO/MonederoDAO.php');
-
-
-if(isset($_POST['modificar'])){
-///////////// Informacion enviada por el formulario /////////////
-    $id=trim($_POST['id']);
-    $concepto = trim($_POST['concepto']);
-    $fecha = trim($_POST['fecha']);
-    $importe = trim($_POST['importe']);
 
 
 
 // PASO 1: RECUPERAR DATOS DEL POST
 
-/*
-if(isset($_POST['concepto'])){
+if(isset($_GET['id'])){
 
-   
-
-}
-
-if(isset($_POST['fecha'])){
-
-  
+   $id = $_GET['id'];
 
 }
 
-if(isset($_POST['importe'])){
+if(isset($_GET['concepto'])){
 
-  
+    $concepto = $_GET['concepto'];
+ 
+ }
 
-}
-*/
+ if(isset($_GET['importe'])){
 
-///////// Fin informacion enviada por el formulario /// 
+    $importe = $_GET['importe'];
+ 
+ }
 
-////////////// Actualizar la tabla /////////
-//INSERT INTO monedero (id, nomCompleto, email, usuario, pass) VALUES (id:, nomCompleto:, email:, usuario:, pass:)
-$consulta = "UPDATE monedero SET `id`=:id, `concepto`= :concepto, `fecha` = :fecha, `importe` = :importe WHERE `id` = :id";
-$sql = $connect->prepare($consulta);
-$sql->bindParam(':concepto',$concepto,PDO::PARAM_STR, 25);
-$sql->bindParam(':fecha',$fecha,PDO::PARAM_STR, 25);
-$sql->bindParam(':importe',$importe,PDO::PARAM_STR,25);
-$sql->bindParam(':id',$id,PDO::PARAM_INT);
+ if(isset($_GET['fecha'])){
 
-$sql->execute();
+    $fecha = $_GET['fecha'];
+ 
+ }
 
-if($sql->rowCount() > 0)
-{
-$count = $sql -> rowCount();
-echo "<div class='content alert alert-primary' > 
+ 
+// PASO 3:CREAR DAO Y LLAMAR A LA FUNCIÓN insertar CON EL DTO DEL PASO 2
 
-Gracias: $count registro ha sido actualizado  </div>";
-}
-else{
-    echo "<div class='content alert alert-danger'> No se pudo actulizar el registro  </div>";
+$registroDAO = new MonederoDAO();
 
-print_r($sql->errorInfo()); 
-}
-}// Cierra envio de guardado
+
+
+$resultado = $registroDAO->modificarRegistro($id);
+
+
+
+echo $resultado;
+
+echo 'Registro modificado';
+
+
+
+header('Location: index.php');
+
+ob_end_flush();
+
+
+
+
+
 ?>
